235a84fe91aa780dc3aa70df25500f696f83e3e8
Savannah Bennett <savvy.bennett8@gmail.com>
2021-04-06 00:09:13 -0600

    api post for new candidates complete

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -75,17 +76,29 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
-// //Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+//Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
